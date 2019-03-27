c75d3db6a7e30ac24b6f2f113c61d73b98d59ec6
israelalagbe <israelalagbe53@gmail.com>
2019-03-27 15:30:17 +0100

    Made little changes to camera activity

--- app/src/main/java/wavetech/facelocker/PinCode.java
+++ app/src/main/java/wavetech/facelocker/PinCode.java
@@ -1,7 +1,10 @@
 package wavetech.facelocker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PinCode extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class PinCode extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_pin_code);
+
+    Button continueButton=findViewById(R.id.continueBtn);
+    continueButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        launchCameraActivity();
+      }
+    });
+  }
+  private void launchCameraActivity(){
+    Intent intent=new Intent(PinCode.this,CameraActivity.class);
+    startActivity(intent);
   }
 }
