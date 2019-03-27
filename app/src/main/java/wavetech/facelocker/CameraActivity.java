d42afe0818060b8b0b0795a24ffce954e4591d7c
israelalagbe <israelalagbe53@gmail.com>
2019-03-27 15:30:17 +0100

    Made little changes to camera activity

---
+++ app/src/main/java/wavetech/facelocker/CameraActivity.java
@@ -0,0 +1,13 @@
+package wavetech.facelocker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CameraActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_camera);
+  }
+}
