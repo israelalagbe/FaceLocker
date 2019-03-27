f9034f7d6a617ecfe8c2ecd32ba8b47718121083
israelalagbe <israelalagbe53@gmail.com>
2019-03-27 14:00:06 +0100

    Password screen design and added button

--- app/src/main/java/wavetech/facelocker/MainActivity.java
+++ app/src/main/java/wavetech/facelocker/MainActivity.java
@@ -1,13 +1,29 @@
 package wavetech.facelocker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
-
+  Switch enableLockSwitch;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    enableLockSwitch=findViewById(R.id.enableLockSwitch);
+    enableLockSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+      @Override
+      public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
+        if(checked){
+          launchPinCodeActivity();
+        }
+      }
+    });
+  }
+  private void launchPinCodeActivity(){
+    Intent intent=new Intent(MainActivity.this,PinCode.class);
+    startActivity(intent);
   }
 }
