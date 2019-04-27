9f88f0fb57b9a0f0534ceac4c422a821e9c251c2
israelalagbe <israelalagbe53@gmail.com>
2019-04-27 14:13:13 +0100

    Moved the dynaic permissions

--- app/src/main/java/wavetech/facelocker/MainActivity.java
+++ app/src/main/java/wavetech/facelocker/MainActivity.java
@@ -1,10 +1,16 @@
 package wavetech.facelocker;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+
+import wavetech.facelocker.utils.LockscreenService;
 
 public class MainActivity extends AppCompatActivity {
   Switch enableLockSwitch;
@@ -17,13 +23,49 @@ public class MainActivity extends AppCompatActivity {
       @Override
       public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
         if(checked){
+          askForPermissions();
           launchPinCodeActivity();
         }
       }
     });
+    startService(new Intent(this, LockscreenService.class));
   }
   private void launchPinCodeActivity(){
     Intent intent=new Intent(MainActivity.this,PinCode.class);
     startActivity(intent);
+  }
+  private void askForPermissions(){
+    askPermissionWithCode(Manifest.permission.CAMERA);
+    askPermissionWithCode(Manifest.permission.READ_EXTERNAL_STORAGE);
+    askPermissionWithCode(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+    askPermissionWithCode(Manifest.permission.DISABLE_KEYGUARD);
+    askPermissionWithCode(Manifest.permission.WAKE_LOCK);
+    askPermissionWithCode(Manifest.permission.RECEIVE_BOOT_COMPLETED);
+    askPermissionWithCode(Manifest.permission.READ_PHONE_STATE);
+    askPermissionWithCode(Manifest.permission.SYSTEM_ALERT_WINDOW);
+  }
+  private void askPermissionWithCode(String code){
+    // Here, thisActivity is the current activity
+    if (ContextCompat.checkSelfPermission(this,
+      code)
+      != PackageManager.PERMISSION_GRANTED) {
+
+      // Permission is not granted
+      // Should we show an explanation?
+      if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+        code)) {
+        // Show an explanation to the user *asynchronously* -- don't block
+        // this thread waiting for the user's response! After the user
+        // sees the explanation, try again to request the permission.
+      } else {
+        // No explanation needed; request the permission
+        ActivityCompat.requestPermissions(this,new String[]{code},80);
+
+
+        // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+        // app-defined int constant. The callback method gets the
+        // result of the request.
+      }
+    }
   }
 }
