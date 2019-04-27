87241f4267734e56eaf7e7974e5ca16c02e8b63f
israelalagbe <israelalagbe53@gmail.com>
2019-04-27 13:40:58 +0100

    Added lock code to lock screen activity

--- app/src/main/java/wavetech/facelocker/LockScreen.java
+++ app/src/main/java/wavetech/facelocker/LockScreen.java
@@ -1,13 +1,197 @@
 package wavetech.facelocker;
 
 import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.KeyguardManager;
+import android.content.Intent;
 import android.os.Bundle;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.WindowManager.LayoutParams;
+import android.widget.Button;
 
-public class LockScreen extends AppCompatActivity {
+import wavetech.facelocker.utils.LockscreenIntentReceiver;
+import wavetech.facelocker.utils.LockscreenService;
+import wavetech.facelocker.utils.LockscreenUtils;
+
+public class LockScreen extends AppCompatActivity
+  implements
+  LockscreenUtils.OnLockStatusChangedListener
+{
+
+  // User-interface
+  private Button btnUnlock;
+
+  // Member variables
+  private LockscreenUtils mLockscreenUtils;
+
+  // Set appropriate flags to make the screen appear over the keyguard
+  @Override
+  public void onAttachedToWindow() {
+    this.getWindow().setType(
+      WindowManager.LayoutParams.TYPE_KEYGUARD_DIALOG);
+    this.getWindow().addFlags(
+      WindowManager.LayoutParams.FLAG_FULLSCREEN
+        | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+        | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+        | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD
+    );
+
+    super.onAttachedToWindow();
+  }
 
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
+  public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
     setContentView(R.layout.activity_lock_screen);
+
+    init();
+
+    // unlock screen in case of app get killed by system
+    if (getIntent() != null && getIntent().hasExtra("kill")
+      && getIntent().getExtras().getInt("kill") == 1) {
+      enableKeyguard();
+      unlockHomeButton();
+    } else {
+
+      try {
+        // disable keyguard
+        disableKeyguard();
+
+        // lock home button
+        lockHomeButton();
+
+        // start service for observing intents
+        startService(new Intent(this, LockscreenService.class));
+
+        // listen the events get fired during the call
+        StateListener phoneStateListener = new StateListener();
+        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+        telephonyManager.listen(phoneStateListener,
+          PhoneStateListener.LISTEN_CALL_STATE);
+
+      } catch (Exception e) {
+      }
+
+    }
+  }
+
+  private void init() {
+    mLockscreenUtils = new LockscreenUtils();
+    btnUnlock = (Button) findViewById(R.id.btnUnlock);
+    btnUnlock.setOnClickListener(new View.OnClickListener() {
+
+      @Override
+      public void onClick(View v) {
+        // unlock home button and then screen on button press
+        unlockHomeButton();
+      }
+    });
+  }
+
+  // Handle events of calls and unlock screen if necessary
+  private class StateListener extends PhoneStateListener {
+    @Override
+    public void onCallStateChanged(int state, String incomingNumber) {
+
+      super.onCallStateChanged(state, incomingNumber);
+      switch (state) {
+        case TelephonyManager.CALL_STATE_RINGING:
+          unlockHomeButton();
+          break;
+        case TelephonyManager.CALL_STATE_OFFHOOK:
+          break;
+        case TelephonyManager.CALL_STATE_IDLE:
+          break;
+      }
+    }
+  };
+
+  // Don't finish Activity on Back press
+  @Override
+  public void onBackPressed() {
+    return;
+  }
+
+  // Handle button clicks
+  @Override
+  public boolean onKeyDown(int keyCode, android.view.KeyEvent event) {
+
+    if ((keyCode == KeyEvent.KEYCODE_VOLUME_DOWN)
+      || (keyCode == KeyEvent.KEYCODE_POWER)
+      || (keyCode == KeyEvent.KEYCODE_VOLUME_UP)
+      || (keyCode == KeyEvent.KEYCODE_CAMERA)) {
+      return true;
+    }
+    if ((keyCode == KeyEvent.KEYCODE_HOME)) {
+
+      return true;
+    }
+
+    return false;
+
+  }
+
+  // handle the key press events here itself
+  public boolean dispatchKeyEvent(KeyEvent event) {
+    if (event.getKeyCode() == KeyEvent.KEYCODE_VOLUME_UP
+      || (event.getKeyCode() == KeyEvent.KEYCODE_VOLUME_DOWN)
+      || (event.getKeyCode() == KeyEvent.KEYCODE_POWER)) {
+      return false;
+    }
+    if ((event.getKeyCode() == KeyEvent.KEYCODE_HOME)) {
+
+      return true;
+    }
+    return false;
+  }
+
+  // Lock home button
+  public void lockHomeButton() {
+    mLockscreenUtils.lock(LockScreen.this);
+  }
+
+  // Unlock home button and wait for its callback
+  public void unlockHomeButton() {
+    mLockscreenUtils.unlock();
+  }
+
+  // Simply unlock device when home button is successfully unlocked
+  @Override
+  public void onLockStatusChanged(boolean isLocked) {
+    if (!isLocked) {
+      unlockDevice();
+    }
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+    unlockHomeButton();
+  }
+
+  @SuppressWarnings("deprecation")
+  private void disableKeyguard() {
+    KeyguardManager mKM = (KeyguardManager) getSystemService(KEYGUARD_SERVICE);
+    KeyguardManager.KeyguardLock mKL = mKM.newKeyguardLock("IN");
+    mKL.disableKeyguard();
+  }
+
+  @SuppressWarnings("deprecation")
+  private void enableKeyguard() {
+    KeyguardManager mKM = (KeyguardManager) getSystemService(KEYGUARD_SERVICE);
+    KeyguardManager.KeyguardLock mKL = mKM.newKeyguardLock("IN");
+    mKL.reenableKeyguard();
+  }
+
+  //Simply unlock device by finishing the activity
+  private void unlockDevice()
+  {
+    finish();
   }
 }
