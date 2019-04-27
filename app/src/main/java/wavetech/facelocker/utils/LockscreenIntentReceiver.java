cd35d8ca78898285be8108bfea3453b4d6814a4e
israelalagbe <israelalagbe53@gmail.com>
2019-04-27 13:19:37 +0100

    Added and cleaned up the utils code

---
+++ app/src/main/java/wavetech/facelocker/utils/LockscreenIntentReceiver.java
@@ -0,0 +1,30 @@
+package wavetech.facelocker.utils;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import wavetech.facelocker.LockScreen;
+
+public class LockscreenIntentReceiver extends BroadcastReceiver {
+
+	// Handle actions and display Lockscreen
+	@Override
+	public void onReceive(Context context, Intent intent) {
+
+		if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)
+				|| intent.getAction().equals(Intent.ACTION_SCREEN_ON)
+				|| intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+			start_lockscreen(context);
+		}
+
+	}
+
+	// Display lock screen
+	private void start_lockscreen(Context context) {
+		Intent mIntent = new Intent(context, LockScreen.class);
+		mIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		context.startActivity(mIntent);
+	}
+
+}
