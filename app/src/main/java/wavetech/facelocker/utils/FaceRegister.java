dae7e4240a0726f5f50251f9e5346a1c7298b030
israelalagbe <israelalagbe53@gmail.com>
2019-05-01 09:36:45 +0100

    Fixed face recognizer dll and wrote a predict method

--- app/src/main/java/wavetech/facelocker/utils/FaceRegister.java
+++ app/src/main/java/wavetech/facelocker/utils/FaceRegister.java
@@ -5,9 +5,13 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.nio.IntBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bytedeco.javacpp.DoublePointer;
+import org.bytedeco.javacpp.IntPointer;
+import org.bytedeco.javacpp.opencv_core;
 import org.opencv.android.Utils;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfInt;
@@ -23,16 +27,37 @@ import android.net.Uri;
 import android.os.Environment;
 import android.util.Log;
 
-import com.googlecode.javacv.cpp.opencv_core;
-import com.googlecode.javacv.cpp.opencv_core.IplImage;
-
-import static com.googlecode.javacv.cpp.opencv_contrib.createEigenFaceRecognizer;
-import static com.googlecode.javacv.cpp.opencv_core.IPL_DEPTH_8U;
-import static com.googlecode.javacv.cpp.opencv_highgui.cvLoadImage;
-import static com.googlecode.javacv.cpp.opencv_imgproc.CV_BGR2GRAY;
-import static com.googlecode.javacv.cpp.opencv_imgproc.cvCvtColor;
+//import com.googlecode.javacv.cpp.opencv_core;
+//import com.googlecode.javacv.cpp.opencv_core.IplImage;
+//
+//import static com.googlecode.javacv.cpp.opencv_contrib.createEigenFaceRecognizer;
+//import static com.googlecode.javacv.cpp.opencv_core.IPL_DEPTH_8U;
+//import static com.googlecode.javacv.cpp.opencv_highgui.cvLoadImage;
+//import static com.googlecode.javacv.cpp.opencv_imgproc.CV_BGR2GRAY;
+//import static com.googlecode.javacv.cpp.opencv_imgproc.cvCvtColor;
+import static org.bytedeco.javacpp.opencv_core.IPL_DEPTH_8U;
+import static org.bytedeco.javacpp.opencv_imgcodecs.CV_LOAD_IMAGE_GRAYSCALE;
+import static org.bytedeco.javacpp.opencv_imgcodecs.cvLoadImage;
+import static org.bytedeco.javacpp.opencv_imgcodecs.imread;
+import static org.bytedeco.javacpp.opencv_imgproc.CV_BGR2GRAY;
+import static org.bytedeco.javacpp.opencv_imgproc.cvCvtColor;
+import static org.opencv.core.CvType.CV_32SC1;
 import static org.opencv.imgcodecs.Imgcodecs.CV_IMWRITE_JPEG_QUALITY;
-import com.googlecode.javacv.cpp.opencv_contrib.FaceRecognizer;
+//import com.googlecode.javacv.cpp.opencv_contrib.FaceRecognizer;
+
+
+
+
+
+//New imports
+import org.bytedeco.javacpp.opencv_face.FaceRecognizer;
+import org.bytedeco.javacpp.opencv_face.FisherFaceRecognizer;
+//import org.bytedeco.javacpp.opencv_core.Mat;
+import org.bytedeco.javacpp.opencv_core.MatVector;
+
+
+
+
 
 import wavetech.facelocker.CameraActivity;
 
@@ -49,24 +74,34 @@ public class FaceRegister{
   }
   public FaceRegister(){
 
-    //faceRecognizer =   new FaceRecognizer();//com.googlecode.javacv.cpp.opencv_contrib.createLBPHFaceRecognizer(2,8,8,8,200);
+    faceRecognizer =   FisherFaceRecognizer.create();//com.googlecode.javacv.cpp.opencv_contrib.createLBPHFaceRecognizer(2,8,8,8,200);
   }
-  public void trainModels() throws IOException{
+  public void predict(Context context,opencv_core.Mat image) throws IOException{
+    IntPointer label = new IntPointer(defaultFaceLabel);
+    DoublePointer confidence = new DoublePointer(1);
+    FaceRecognizer faceRecognizer = FisherFaceRecognizer.create();
+    faceRecognizer.predict(image, label, confidence);
+    int predictedLabel = label.get(0);
+    Log.v(TAG,"Predicted Label: "+predictedLabel);
+  }
+  public void trainModels(Context context) throws IOException{
     FilenameFilter fileNameFilter = new FilenameFilter() {
       public boolean accept(File dir, String name) {
-        return name.toLowerCase().endsWith(".jpg");
+        return name.toLowerCase().endsWith(".jpg")||name.toLowerCase().endsWith(".jpeg");
 
       };
     };
-    File path = new File("/sdcard/opencv/capture");
+    File path = context.getFilesDir();
     File[] capturedImages=path.listFiles(fileNameFilter);
-    int[] labels = new int[capturedImages.length];
-    opencv_core.MatVector images = new opencv_core.MatVector(capturedImages.length);
+    opencv_core.Mat labels = new opencv_core.Mat(capturedImages.length, 1, CV_32SC1);
+    //int[] labels = new int[capturedImages.length];
+    MatVector images = new MatVector(capturedImages.length);
+    IntBuffer labelsBuf = labels.createBuffer();
     for (int i=0;i<capturedImages.length;i++)
     {
 
       File imageFile=capturedImages[i];
-      IplImage img = cvLoadImage(imageFile.getAbsolutePath());
+      opencv_core.Mat img = imread(imageFile.getAbsolutePath(), CV_LOAD_IMAGE_GRAYSCALE);
 
       if(img==null){
         throw  new IOException("Unable to load CV IMage from path: "+imageFile.getAbsolutePath());
@@ -74,17 +109,26 @@ public class FaceRegister{
 
 
 
-      IplImage grayImg = IplImage.create(img.width(), img.height(), IPL_DEPTH_8U, 1);
-      cvCvtColor(img, grayImg, CV_BGR2GRAY);
-      images.put(i, grayImg);
+//      opencv_core.IplImage grayImg = opencv_core.IplImage.create(img.width(), img.height(), IPL_DEPTH_8U, 1);
+//      cvCvtColor(img, grayImg, CV_BGR2GRAY);
+//      images.put
+      labelsBuf.put(i, defaultFaceLabel);
+      images.put(i, img);
       //Label corresponding to the id of t
       // he image to be used for recognizing which image it is,
       // I'd be using ID 1 for now
-      labels[i]=defaultFaceLabel;
-      faceRecognizer.train(images,labels);
-      faceRecognizer.save("/sdcard/opencv/result");
+      //labels.put(img);
+      //labels[i]=defaultFaceLabel;
+
 
     }
+    FaceRecognizer faceRecognizer = FisherFaceRecognizer.create();
+    faceRecognizer.train(images,labels);
+    File targetFile=new File(path, "train.xml");
+    faceRecognizer.save(targetFile.getAbsolutePath());
+    Log.v(TAG,"Saved: "+targetFile.exists());
+    opencv_core.Mat testImage=imread(capturedImages[0].getAbsolutePath(), CV_LOAD_IMAGE_GRAYSCALE);
+    this.predict(context,testImage);
   }
   private void saveMatToImg(Context context,Mat mat) throws IOException {
 
