7223499f27af961fbd1be4215a07627d39be54bb
israelalagbe <israelalagbe53@gmail.com>
2019-06-15 13:25:17 +0100

    Added a tutorial activity for tour guide

---
+++ app/src/main/java/wavetech/facelocker/Tutorial.java
@@ -0,0 +1,13 @@
+package wavetech.facelocker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Tutorial extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_tutorial);
+  }
+}
